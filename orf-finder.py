7aff23bab1257213fb678f4f8b5d411915af56d3
alexarlord-boop <rezerw0.1@yandex.ru>
2022-03-21 12:36:04 +0300

    BIO-2022 Added data parsing func

--- orf-finder.py
+++ orf-finder.py
@@ -2,23 +2,43 @@
 # 2. extracting translations for each fragments
 # 3. extracting ORF for each translation
 
-import Utils
+from Utils import IOUtils
 from msr import MatrixSynthReaction
 
-
-WORK_PATH = "C:\\bio\\orf-finder"
+WORK_PATH = "C:\\bio\\orf-finder\\"
 
 
 class OrfFinder:
-    def __init__(self, src_path, snk_path):
+    def __init__(self, src_path, snk_path, data):
         self.data_src = src_path
         self.data_snk = snk_path
+        self.data = data
 
         self.data_fragments = []
         self.potential_genes = []
 
-    def get_fragments_from_data(self):
-        pass
+    def set_data_fragments(self, fragments):
+        self.data_fragments = fragments
+
+    def get_data_fragments(self):
+        return self.data_fragments
+
+    def parse_fragments_from_data(self):
+        d = len(self.data)
+        header = None
+        fragments = []
+        seq = ''
+        for i in range(d):
+            line = self.data[i][:-1]
+            if line[0] == '>':
+                if header:
+                    fragments.append((header, seq))
+                header = str(line[1:])
+                seq = ""
+            else:
+                seq += line
+        fragments.append((header, seq))
+        self.set_data_fragments(fragments)
 
     def get_translation_from_fragment(self, fragment, shift):
         """
@@ -31,3 +51,14 @@ class OrfFinder:
         pass
 
 
+if __name__ == '__main__':
+    src_path = WORK_PATH + "dna-seq.fna"
+    snk_path = WORK_PATH + "orfs.txt"
+    io = IOUtils()
+    data = io.open_file(src_path)
+    orf_finder = OrfFinder(src_path, snk_path, data)
+
+    orf_finder.parse_fragments_from_data()
+    fragments = orf_finder.get_data_fragments()
+    # print(*fragments, sep='\\n')
+    print(fragments[0][1])
